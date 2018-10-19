52047a327e4c23c2c10b03041fb115b6b1a5641a
bbqcake <tdarkture@gmail.com>
2018-10-19 23:36:31 +0200

    Added sound

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidBody;
+	AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+		
 	}
 	
 	// Update is called once per frame
@@ -23,15 +26,26 @@ public class Rocket : MonoBehaviour {
 		if (Input.GetKey(KeyCode.Space))
 		{
 			rigidBody.AddRelativeForce(Vector3.up);
-			
+			audioSource.mute = false;
+
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			} 
 		}
+		else
+		{ 
+			audioSource.mute = true;
+		}		
 		if (Input.GetKey(KeyCode.A))
 		{
 			transform.Rotate(Vector3.forward);
+			
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
 			transform.Rotate(-Vector3.forward);
+			
 		}
 		
 
