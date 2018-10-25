c8c23ab4722c8918046196ce2d711314e3bf1403
bbqcake <tdarkture@gmail.com>
2018-10-25 15:52:00 +0200

    Tiny backup

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,54 +1,144 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
+	// TODO fix lighting big
+	[SerializeField] float rcsThrust = 125f;
+	[SerializeField] float mainThrust = 100f;
+	[SerializeField] AudioClip MainEngineSFX;
+	[SerializeField] AudioClip DeathSFX;
+	[SerializeField] AudioClip SuccessSFX;
+
+
 	Rigidbody rigidBody;
 	AudioSource audioSource;
+
+	enum State { Alive, Dying, Transcending }
+		State state = State.Alive;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rigidBody = GetComponent<Rigidbody>();
-		audioSource = GetComponent<AudioSource>();
+		audioSource = GetComponent<AudioSource>();		
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		ProcessInput();
+		// TODO stop sound on death
+		if (state == State.Alive)
+		{
+		RespontToRotateInput();
+		RespondToThrustInput();
+		}
+		
 	}
 
-	private void ProcessInput()
+	private void RespontToRotateInput()
 	{
+		rigidBody.freezeRotation = true; //manual rotation of rotation
+
+		float roationThisFrame = rcsThrust * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A))
+		{			
+			transform.Rotate(Vector3.forward * roationThisFrame);			
+		}		
+		else if (Input.GetKey(KeyCode.D))
+		{			
+			transform.Rotate(-Vector3.forward * roationThisFrame);			
+		}
+
+		rigidBody.freezeRotation = false; // resume physics control of rotation
+	}	
+
+
+	 void OnCollisionEnter(Collision collision)	
+	 {
+
+		 if (state != State.Alive)
+		 {
+			 return; // Ignores collisions when dead
+		 }
+
+		 switch (collision.gameObject.tag)
+		 {
+			case "Friendly":
+				// do nothing
+				print("safe to land here");
+				break;
+
+			case "Finish":
+				
+				StartSuccessSequence();
+				break;
+
+			default:
+				StartDeathSequence();			
+				break;
+
+		 }
+	 }
+
+	 private void StartSuccessSequence()
+	 {
+		state = State.Transcending;	
+		audioSource.Stop();
+		audioSource.PlayOneShot(SuccessSFX);			
+		Invoke("LoadNextLevel", 1f); // load after one second
+	 }
+
+	 private void StartDeathSequence()
+	 {
+		state = State.Dying;
+		audioSource.Stop();
+		audioSource.PlayOneShot(DeathSFX);			
+		Invoke("LoadFirstLevel", 1f);		
+	 }
+
+	 private void LoadNextLevel()
+	 {
+		 //TODO allow more levels
+		 SceneManager.LoadScene(1);
+	 }
+
+	 private void LoadFirstLevel()
+	 {
+		 SceneManager.LoadScene(0);
+	 }
+
+
+
+	private void RespondToThrustInput()
+	{		
 		if (Input.GetKey(KeyCode.Space))
-		{
-			rigidBody.AddRelativeForce(Vector3.up);
+		{			
+			ApplyThrust();
+		}
+		else
+		{ 
+			audioSource.mute = true; //TODO only mute engine
+		}	
+	}
+
+	private void ApplyThrust()
+	{
+		rigidBody.AddRelativeForce(Vector3.up * mainThrust);
 			audioSource.mute = false;
 
 			if (!audioSource.isPlaying)
 			{
-				audioSource.Play();
+				audioSource.PlayOneShot(MainEngineSFX);
 			} 
-		}
-		else
-		{ 
-			audioSource.mute = true;
-		}		
-		if (Input.GetKey(KeyCode.A))
-		{
-			transform.Rotate(Vector3.forward);
-			
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			transform.Rotate(-Vector3.forward);
-			
-		}
-		
-
-
 	}
+
 }
+
+
+
+
